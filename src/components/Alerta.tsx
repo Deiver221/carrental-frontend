f6d6ead1a66212abe37bd7408db8a9fb575ceb88
Deiverrr <delvin2812@gmail.com>
2026-03-23 13:28:30 -0300

    Fix Alerta component

--- src/components/Alerta.tsx
+++ src/components/Alerta.tsx
@@ -1,7 +1,13 @@
-export default function Alerta({children}) {
+import { type ReactNode } from "react"
+
+interface AlertaProps {
+  children: ReactNode
+}
+
+export default function Alerta({ children }: AlertaProps) {
   return (
-    <div className="text-center my-2 bg-red-500/30 border-2 border-red-600  text-white  font-semibold p-3 rounded-lg">
-        {children}
+    <div className="text-center my-2 bg-red-500/30 border-2 border-red-600 text-white font-semibold p-3 rounded-lg">
+      {children}
     </div>
   )
 }
